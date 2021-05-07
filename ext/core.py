1a7203325dfcb9fc648ed7c51df80a94c9e73d57
janu8ry <janu8ry0108@gmail.com>
2021-05-07 11:17:41 +0900

    changed version

--- ext/core.py
+++ ext/core.py
@@ -17,7 +17,7 @@ from ext.db import config, db, dbconfig, password, username, write
 
 
 class Kkutbot(commands.AutoShardedBot):
-    __version__ = "1.7.0a"
+    __version__ = "1.7.0-alpha.0"
     __slots__ = ("db", "dblpy", "koreanbots", "uniquebots", "webhook", "hanmaru", "scheduler")
     description = "끝봇은 끝말잇기가 주 기능인 인증된 디스코드 봇입니다."
     version_info = "개발중"
