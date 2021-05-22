dfab79caae9ecdf46e3ece55374370ca295eb934
janu8ry <janu8ry0108@gmail.com>
2021-05-22 11:17:30 +0900

    isort

--- ext/db.py
+++ ext/db.py
@@ -1,11 +1,11 @@
+import asyncio
 from copy import deepcopy
 from datetime import datetime
 from typing import Optional
-import asyncio
 
 import discord
-from motor.motor_asyncio import AsyncIOMotorClient
 import uvloop
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from ext.config import config
 from ext.config import get_nested_dict as get
