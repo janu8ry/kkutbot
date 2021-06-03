60dab1e320ce33376b78490071d8ea915da21fc6
janu8ry <janu8ry0108@gmail.com>
2021-06-03 19:35:49 +0900

    remove unused codes

--- main.py
+++ main.py
@@ -278,5 +278,3 @@ async def on_guild_remove(guild: discord.Guild):
 
 print("로그인하는 중...")
 bot.run(config(f"token.{'test' if config('test') else 'main'}"))  # todo: 모든 명령어 도움말 웹사이트에 추가
-asyncio.run(bot.webhook.close())
-# asyncio.run(bot.http._HTTPClient__session.close())  # noqa
