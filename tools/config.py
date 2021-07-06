08e933409ac6e4888d8ea339fc151a724435f988
janu8ry <janu8ry0108@gmail.com>
2021-07-06 15:07:10 +0900

    feat: add config file parser

---
+++ tools/config.py
@@ -0,0 +1,49 @@
+import json
+import os
+from typing import Optional, List, Any
+
+import yaml
+
+with open('config.yml', encoding='utf-8') as f:
+    config_data = yaml.load(f, Loader=yaml.FullLoader)
+
+for file in os.listdir('data'):
+    if file not in ("wordlist.json", "DUlaw.json", "quest.json"):
+        with open(f"data/{file}", 'r', encoding="utf-8") as f:
+            config_data[file[:-5]] = json.load(f)
+
+
+def get_nested_dict(data: dict, path: List[str]) -> Any:
+    """
+    gets value in nested dictionary.
+    Parameters
+    ----------
+    data : dict
+        Target dictionary to get value
+    path : list[str]
+        list of keys to get value
+    Returns
+    -------
+    Any
+        value in targeted dictionary
+    """
+    for i in path:
+        data = data.get(i, None)
+    return data
+
+
+def config(query: Optional[str] = None) -> Any:
+    """
+    gets value in 'config.yml' file
+    Parameters
+    ----------
+    query : Optional[str]
+    Returns
+    -------
+    Any
+        value in 'config.yml' file
+    """
+    if not query:
+        return config_data
+    else:
+        return get_nested_dict(config_data, query.split('.'))
