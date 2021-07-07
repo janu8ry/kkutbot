a9962a3e20fa53a693b39f4fcbec931e8bd53bb0
janu8ry <janu8ry0108@gmail.com>
2021-07-07 23:40:40 +0900

    feat: add directory setting option & cli

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 import asyncio
+import os
+import sys
+import warnings
 
 from tools.db import db
 
 
-async def main():
+async def mode1():
     await db.user.update_many({}, {
         "$rename": {
             "_name": "name",
@@ -57,5 +60,30 @@ async def main():
         }
     })
 
+
+def mode2():
+    os.mkdir("logs")
+    os.mkdir("backup")
+
+
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+    mode = ""
+    if len(sys.argv) == 2:
+        mode = sys.argv[1]
+    while True:
+        if mode:
+            mode = input("""
+  끝봇 설정
+============
+1. DB 업데이트
+2. 디렉토리 세팅
+============
+>>> """)
+        if mode == "1":
+            asyncio.get_event_loop().run_until_complete(mode1())
+            sys.exit()
+        elif mode == "2":
+            mode2()
+            sys.exit()
+        else:
+            warnings.warn("잘못된 모드입니다. 다시 선택해 주세요.")
