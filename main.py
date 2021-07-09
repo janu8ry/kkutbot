e57b1516989508b64651fb69d68cc08ca15294a9
janu8ry <janu8ry0108@gmail.com>
2021-07-09 09:28:24 +0900

    feat: install rich traceback

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import logging
 
+from rich import traceback
+
 from tools.logger import setup_logger
 
 logger = logging.getLogger("kkutbot")
 
 if __name__ == "__main__":
+    traceback.install()
     setup_logger()
