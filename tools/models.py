cdc82a043910e6e19078eb353348b0873ff62b6b
janu8ry <janu8ry0108@gmail.com>
2021-07-13 00:12:14 +0900

    BREAKING CHANGE: rewrite all classes

--- tools/models.py
+++ tools/models.py
@@ -1,163 +1,133 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from datetime import datetime
+import inspect
+import asyncio
+
+from .db import write
 
 __all__ = ("UserModel", "GuildModel", "GeneralModel")
 
 
 @dataclass
-class UserQuestStatusModel:
-    date: int = 0
-    completed: list = field(default_factory=list)
+class BaseModel:
+    _id: int
+    _col: str
 
-    @classmethod
-    def from_dict(cls, data: dict):
-        return cls(**data)
-
-
-@dataclass
-class UserQuestModel:
-    status: UserQuestStatusModel = UserQuestStatusModel()
-    cache: dict = field(default_factory=dict)
-
-    @classmethod
-    def from_dict(cls, data: dict):
-        return cls(
-            status=UserQuestStatusModel.from_dict(data=data.get("status")),
-            cache=data.get("cache"),
-        )
+    def __setattr__(self, key, value):
+        if not inspect.stack()[1].function == "__init__":
+            asyncio.get_event_loop().create_task(
+                write(self._id, self._col, f"{self._path}.{key}", value)  # noqa
+            )
+        super().__setattr__(key, value)
 
 
 @dataclass
-class BaseGameModel:
-    times: int = 0
-    win: int = 0
-    best: int = 0
-    winrate: int = 0
+class UserQuestStatusModel(BaseModel):
+    date: int
+    completed: list
+    _path: str = "quest.status"
 
-    @classmethod
-    def from_dict(cls, data: dict):
-        return cls(**data)
+
+@dataclass
+class UserQuestModel(BaseModel):
+    status: UserQuestStatusModel
+    cache: dict
+    _path: str = "quest"
+
+
+@dataclass
+class BaseGameModel(BaseModel):
+    times: int
+    win: int
+    best: int
+    winrate: int
 
 
 @dataclass
 class BaseRankGameModel(BaseGameModel):
-    tier: str = "언랭크"
+    tier: str
 
 
 @dataclass
 class SoloRankGameModel(BaseRankGameModel):
-    pass
+    _path: str = "game.rank_solo"
 
 
 @dataclass
 class OnlineRankGameModel(BaseRankGameModel):
-    pass
+    _path: str = "game.rank_online"
 
 
 @dataclass
 class KkdGameModel(BaseGameModel):
-    pass
+    _path: str = "game.kkd"
 
 
 @dataclass
 class GuildGameModel(BaseGameModel):
-    pass
+    _path: str = "game.guild_multi"
 
 
 @dataclass
 class MultiOnlineGameModel(BaseGameModel):
-    pass
+    _path: str = "game.online_multi"
 
 
 @dataclass
 class ApmalGameModel(BaseGameModel):
-    pass
+    _path: str = "game.apmal"
 
 
 @dataclass
-class UserGameStatusModel:
-    rank_solo: SoloRankGameModel = SoloRankGameModel()
-    rank_online: OnlineRankGameModel = OnlineRankGameModel()
-    kkd: KkdGameModel = KkdGameModel()
-    guild_multi: GuildGameModel = GuildGameModel()
-    online_multi: MultiOnlineGameModel = MultiOnlineGameModel()
-    apmal: ApmalGameModel = ApmalGameModel()
-
-    @classmethod
-    def from_dict(cls, data):
-        return cls(
-            rank_solo=SoloRankGameModel.from_dict(data.get("rank_solo")),
-            rank_online=OnlineRankGameModel.from_dict(data.get("rank_online")),
-            kkd=KkdGameModel.from_dict(data.get("kkd")),
-            guild_multi=GuildGameModel.from_dict(data.get("guild_multi")),
-            online_multi=MultiOnlineGameModel.from_dict(data.get("online_multi")),
-            apmal=ApmalGameModel.from_dict(data.get("apmal")),
-        )
+class UserGameStatusModel(BaseModel):
+    rank_solo: SoloRankGameModel
+    rank_online: OnlineRankGameModel
+    kkd: KkdGameModel
+    guild_multi: GuildGameModel
+    online_multi: MultiOnlineGameModel
+    apmal: ApmalGameModel
+    _path: str = "game"
 
 
 @dataclass
-class AlertModel:
-    daily: int = None
-    heart: int = None
-    mail: int = None
-
-    @classmethod
-    def from_dict(cls, data: dict):
-        return cls(**data)
+class AlertModel(BaseModel):
+    reward: int
+    mail: int
+    _path: str = "alerts"
 
 
 @dataclass
-class UserModel:
-    id: int = None
-    name: str = None
-    registered: datetime = None
-    info: str = "소개말이 없습니다."
-    points: int = 1000
-    medals: int = 0
-    latest_reward: int = None
-    quest: UserQuestModel = UserQuestModel()
-    game: UserGameStatusModel = UserGameStatusModel()
-    reward_times: int = 0
-    command_used: int = 0
-    banned: bool = False
-    latest_usage: int = None
-    mails: list = field(default_factory=list)
-    alerts: AlertModel = AlertModel()
-
-    @classmethod
-    def from_dict(cls, data: dict):
-        return cls(
-            id=data.get("_id"),
-            name=data.get("name"),
-            registered=data.get("registered"),
-            info=data.get("info"),
-            points=data.get("points"),
-            medals=data.get("medals"),
-            latest_reward=data.get("latest_reward"),
-            quest=UserQuestModel.from_dict(data.get("quest")),
-            game=UserGameStatusModel.from_dict(data.get("game")),
-            reward_times=data.get("attendance_times"),
-            command_used=data.get("command_used"),
-            banned=data.get("banned"),
-            latest_usage=data.get("latest_usage"),
-            mails=data.get("mails"),
-            alerts=AlertModel.from_dict(data.get("alerts")),
-        )
+class UserModel(BaseModel):
+    _id: int
+    name: str
+    registered: datetime
+    info: str
+    points: int
+    medals: int
+    latest_reward: int
+    quest: UserQuestModel
+    game: UserGameStatusModel
+    reward_times: int
+    command_used: int
+    banned: bool
+    latest_usage: float
+    mails: list
+    alerts: AlertModel
 
 
 @dataclass
 class GuildModel:
-    id: int = None
-    invited: int = None
-    latest_usage: int = None
-    command_used: int = 0
+    _id: int
+    invited: int
+    latest_usage: float
+    command_used: int
 
 
 @dataclass
 class GeneralModel:
-    id: str = "general"
-    attendance: int = 0
-    command_used: int = 0
-    latest_command = None
-    commands: dict = field(default_factory=dict)
-    quests: dict = field(default_factory=dict)
+    _id: str
+    attendance: int
+    command_used: int
+    latest_command: float
+    commands: dict
+    quests: dict
