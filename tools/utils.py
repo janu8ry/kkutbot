edd060be368826c1305c90d03d029bda7786cd03
janu8ry <janu8ry0108@gmail.com>
2022-06-03 00:55:39 +0900

    feat: added time conversion function

---
+++ tools/utils.py
@@ -0,0 +1,14 @@
+from datetime import timedelta
+from typing import Union
+
+
+def time_convert(time: Union[int, float, timedelta]) -> str:
+    if isinstance(time, (int, float)):
+        time = timedelta(seconds=time)
+    if time.days > 0:
+        return f"{time.days}일"
+    if time.seconds >= 3600:
+        return f"{time.seconds // 3600}시간"
+    if time.seconds >= 60:
+        return f"{time.seconds // 60}분"
+    return f"{time.seconds}초"
