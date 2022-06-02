60b568741b9e4679fd82d0e02419c33fa5162a10
janu8ry <janu8ry0108@gmail.com>
2022-06-03 00:37:06 +0900

    feat: remove old mails

--- tools/db.py
+++ tools/db.py
@@ -179,6 +179,10 @@ async def write(target: TargetObject, path: str, value):
                 main_data["_id"] = id_
                 main_data["name"] = name
                 await db.user.insert_one(main_data)
+        mails: list = await read(target, "mails")
+        for mail in deepcopy(mails):
+            if (datetime.now() - mail['time']).days > 14:
+                mails.remove(mail)
         if name and name != (await read(target, "name")):
             await db.user.update_one({"_id": id_}, {"$set": {"name": name}})
     elif (collection.name == "guild") and (await read(target) == deepcopy(config("default_data.guild"))):
