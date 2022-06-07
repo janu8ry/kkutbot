b03af2801ddeacb0be4dc0401f6659f4ed32e6fd
janu8ry <janu8ry0108@gmail.com>
2022-06-07 15:41:17 +0900

    fix: typo

--- cogs/admin.py
+++ cogs/admin.py
@@ -50,7 +50,7 @@ class Admin(commands.Cog, name="관리자"):
             value=f"서버: `{len(self.bot.guilds)}`개\n"
                   f"유저: `{await self.bot.db.user.count_documents({})}`명\n"
                   f"미사용 유저: `{await self.bot.db.unused.count_documents({})}`명\n"
-                  f"활성화: `{await self.bot.db.user.count_documents({'latest_command': {'$gte': round(time.time() - 86400 * count)}})}`명\n"
+                  f"활성화: `{await self.bot.db.user.count_documents({'latest_usage': {'$gte': round(time.time() - 86400 * count)}})}`명\n"
                   f"출석 유저 수: `{await read(None, 'attendance')}`명"
         )
         embed.add_field(
