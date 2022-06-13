ef34318782ef06f3bb6d2e9aff77d3154bf5aaaf
janu8ry <janu8ry0108@gmail.com>
2022-06-14 00:56:57 +0900

    feat: add page index input

--- tools/paginator.py
+++ tools/paginator.py
@@ -3,7 +3,26 @@ from typing import List
 import discord
 from discord.ext import commands
 
-from tools.views import DefaultView
+from tools.views import DefaultView, DefaultModal
+
+
+class PageInput(DefaultModal, title="페이지 이동하기"):
+    target_page = discord.ui.TextInput(label='페이지 번호', placeholder="이동할 페이지의 번호를 입력해 주세요.", required=True)
+
+    def __init__(self, ctx: commands.Context, view: "Paginator"):
+        super().__init__()
+        self.ctx = ctx
+        self.target = None
+        self.view = view
+        self.target_page.label = f"페이지 번호 (1~{view.page_count})"
+
+    async def on_submit(self, interaction: discord.Interaction):
+        if self.target_page.value.isdecimal() and (1 <= int(self.target_page.value) <= self.view.page_count):
+            self.view.index = int(self.target_page.value) - 1
+            await self.view.children[2].update_buttons(interaction)
+        else:
+            await interaction.response.send_message(f"올바른 값이 아닙니다.\n가능한 값: (1~{self.view.page_count})", ephemeral=True)
+            return self.stop()
 
 
 class PaginatorButton(discord.ui.Button):
@@ -34,9 +53,13 @@ class ToBack(PaginatorButton):
         await self.update_buttons(interaction)
 
 
-class PageInfo(discord.ui.Button):
+class PageInfo(PaginatorButton):
     def __init__(self, pagecount):
-        super().__init__(label=f"1/{pagecount}", style=discord.ButtonStyle.gray, disabled=True)
+        super().__init__(label=f"1/{pagecount}", style=discord.ButtonStyle.gray, disabled=False)
+
+    async def callback(self, interaction: discord.Interaction):
+        modal = PageInput(self.view.ctx, self.view)
+        await interaction.response.send_modal(modal)
 
 
 class ToNext(PaginatorButton):
