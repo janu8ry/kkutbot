a5937fece244be105e1ba62dc7c2426fc7193ba2
janu8ry <janu8ry@users.noreply.github.com>
2022-09-04 09:44:39 +0000

    style(isort): sort imports

--- cogs/game.py
+++ cogs/game.py
@@ -7,8 +7,8 @@ from typing import Literal, Union
 import discord
 from discord.ext import commands
 
-from core import Kkutbot, KkutbotContext
 from config import config
+from core import Kkutbot, KkutbotContext
 from tools.db import add, read, write
 from tools.utils import choose_first_word, get_tier, get_transition, get_winrate, get_word, is_hanbang
 from views.game import HostGuildGame, SelectMode
