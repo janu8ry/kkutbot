d999e66670a4cd5f609d30508d7f54aeefe09b9f
janu8ry <janu8ry0108@gmail.com>
2023-07-08 14:47:00 +0900

    feat: add basic events

--- main.py
+++ main.py
@@ -1,11 +1,73 @@
 import logging
+import time
+from datetime import timedelta, timezone
 
+import discord
 from rich.traceback import install as rich_install
 
+import core
+from config import config
 from tools.logger import setup_logger
 
 logger = logging.getLogger("kkutbot")
 
+bot = core.Kkutbot()
+
+
+@bot.event
+async def on_ready() -> None:
+    await bot.reload_all()  # TODO: jishaku
+
+    guilds = len(bot.guilds)
+    users = await bot.db.client.user.count_documents({})
+
+    logger.info(f"'{bot.user.name}'으로 로그인됨\n"
+                f"서버수: {guilds}, 유저수: {users}")
+
+    await bot.update_presence()
+
+
+@bot.event
+async def on_shard_ready(shard_id: int) -> None:
+    logger.info(f"{shard_id}번 샤드 준비 완료!")
+
+
+@bot.check
+async def check(ctx: core.KkutbotContext) -> bool:
+    if ctx.guild and not ctx.channel.permissions_for(ctx.guild.me).send_messages:
+        try:
+            embed = discord.Embed(
+                title="오류",
+                description=f"{ctx.channel.mention}에서 끝봇에게 메시지 보내기 권한이 없어서 명령어를 사용할 수 없습니다.\n"
+                            f"끝봇에게 해당 권한을 지급한 후 다시 시도해주세요.",
+                color=config.colors.error
+            )
+            await ctx.author.send(embed=embed)
+        except discord.Forbidden:
+            pass
+        return False
+
+    return True
+
+
+@bot.event
+async def on_interaction(interaction: discord.Interaction) -> None:
+    if interaction.type == discord.InteractionType.component:
+        kst = timezone(timedelta(hours=9))
+        interaction_created = round(time.mktime(interaction.message.created_at.astimezone(kst).timetuple()))
+        if interaction_created < bot.started_at:
+            types = ["그룹은", "버튼은", "리스트는", "텍스트박스는"]
+            await interaction.response.send_message(
+                embed=discord.Embed(
+                    description=f"{{denyed}} 이 {types[interaction.data['component_type'] - 1]} 너무 오래되어 사용할 수 없어요.\n"
+                                f"명령어를 새로 입력해주세요.",
+                    color=config.colors.error
+                ),
+                ephemeral=True
+            )
+
+
 if __name__ == "__main__":
     rich_install()
     setup_logger()
+    bot.run_bot()
