852902b65f24b7da61e50c1249b868ac2190fc69
janu8ry <janu8ry0108@gmail.com>
2023-07-08 17:13:48 +0900

    feat: before command action

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import logging
+import random
 import time
-from datetime import timedelta, timezone
+import traceback
+import uuid
+from datetime import datetime, timedelta, timezone
+from typing import Type, Union
 
 import discord
+from discord.ext import commands
 from rich.traceback import install as rich_install
+from beanie.operators import Push
 
 import core
-from config import config
+from config import config, get_nested_dict
 from tools.logger import setup_logger
+from tools.utils import is_admin
+from views.general import ServerInvite
+from database.models import User
 
 logger = logging.getLogger("kkutbot")
 
@@ -38,6 +47,48 @@ async def on_ready() -> None:
 @bot.event
 async def on_shard_ready(shard_id: int) -> None:
     logger.info(f"{shard_id}번 샤드 준비 완료!")
+
+
+@bot.before_invoke
+async def before_command(ctx: core.KkutbotContext) -> None:
+    user = await bot.db.get_user(ctx.author)
+    user.command_used += 1
+    user.latest_usage = round(time.time())
+
+    if ctx.guild:
+        guild = await bot.db.get_guild(ctx.guild)
+        guild.latest_usage = round(time.time())
+        guild.command_used += 1
+        await bot.db.save(guild)
+
+    public = await bot.db.get_public()
+    public.command_used += 1
+    public.latest_usage = round(time.time())
+    cmd_name = ctx.command.qualified_name.replace('$', '_')  # TODO: 뭔지모를 버그 해결
+    if cmd_name in public.commands:
+        public.commands[cmd_name] += 1
+    else:
+        public.commands[cmd_name] = 1
+
+    if user.quest.status.date != (today := datetime.today().toordinal()):
+        user.quest.status.date = today
+        user.quest.status.completed = []
+        cache = {}
+        for data in public.quests.keys():
+            cache[data] = get_nested_dict(user.dict(), data.split("/"))
+        user.quest.cache = cache
+
+    await bot.db.save(user)
+    await bot.db.save(public)
+
+    if ctx.message.content:
+        msg = ctx.message.content
+    else:
+        msg = f"/{ctx.command}"
+    if isinstance(ctx.channel, discord.DMChannel):
+        logger.command(f"{ctx.author} [{ctx.author.id}]  |  DM [{ctx.channel.id}]  |  {msg}")
+    else:
+        logger.command(f"{ctx.author} [{ctx.author.id}]  |  {ctx.guild} [{ctx.guild.id}]  |  {ctx.channel} [{ctx.channel.id}]  |  {msg}")
 
 
 @bot.check
