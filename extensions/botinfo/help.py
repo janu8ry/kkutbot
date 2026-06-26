9689523c18bb20d8a63c8dec23fc0e16f7448bf6
janu8ry <janu8ry0108@gmail.com>
2026-06-26 21:12:45 +0900

    refactor: adjust help message

--- extensions/botinfo/help.py
+++ extensions/botinfo/help.py
@@ -27,8 +27,8 @@ class Help(commands.Cog, name="일반"):
         embed = discord.Embed(
             title=fmt("{help} 끝봇 도움말"),
             description="🔸 끝봇은 끝말잇기가 주 기능인 디스코드 봇입니다!\n\n"
-            f"🔹 **개발자**: `{(await self.bot.application_info()).owner}`\n"
-            f"🔹 **서버 /사용자 수**: `{len(self.bot.guilds)}`개/`{await User.count()}`명\n"
+            f"🔹 **개발자**: `{(await self.bot.application_info()).team.owner}`\n"
+            f"🔹 **서버 / 사용자 수**: `{len(self.bot.guilds)}`개 / `{await User.count()}`명\n"
             f"🔹 **업타임**: ~ <t:{self.bot.started_at}:R>부터\n\n"
             "개발에 도움을 주신 `seojin_park`님,\n프로필 사진을 만들어 주신 `! Tim23#1475` 님께 감사드립니다!\n"
             "Icon made from [flaticon](https://www.flaticon.com)",
@@ -36,7 +36,7 @@ class Help(commands.Cog, name="일반"):
         )
         embed.add_field(
             name="🔹 기타 링크",
-            value=f"[웹사이트]({config.links.website})  [koreanbots]({config.links.koreanbots})  [github]({config.links.github})  [개인정보처리방침]({config.links.privacy_policy})",
+            value=f"[웹사이트]({config.links.website}) | [Koreanbots]({config.links.koreanbots}) | [Github]({config.links.github}) | [개인정보처리방침]({config.links.privacy_policy})",
         )
         embed.set_thumbnail(url=self.bot.user.display_avatar.url)
         embed.set_footer(text="아래 메뉴를 클릭해서 명령어 도움말을 확인해 보세요!")
